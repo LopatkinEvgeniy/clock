65f86fdaebd1a27e9d28bde27c47de1e7f83fd1d
elopatkin <elopatkin@tradingview.com>
2018-10-05 18:19:53 +0300

    add test for FakeClock:After function

--- clock_test.go
+++ clock_test.go
@@ -60,3 +60,34 @@ func TestFakeClockNowStress(t *testing.T) {
 		t.Fatalf("unexpected now result, expected: %s, actual: %s", expected, now)
 	}
 }
+
+func TestFakeClockAfter(t *testing.T) {
+	c := clock.NewFakeClock()
+	ch := c.After(100 * time.Minute)
+
+	for i := 0; i < 99; i++ {
+		c.Add(time.Minute)
+
+		select {
+		case <-ch:
+			t.Fatal("Unexpected channel receive")
+		default:
+		}
+	}
+
+	c.Add(time.Minute)
+	var actualTime time.Time
+
+	select {
+	case actualTime = <-ch:
+	default:
+		t.Fatal("Expected channel receive")
+	}
+
+	expectedTime := (time.Time{}).Add(100 * time.Minute)
+	if expectedTime != actualTime {
+		t.Fatalf("Unexpected time received from the channel, expected=%s, actual=%s", expectedTime, actualTime)
+	}
+}
+
+// TODO: AfterFunc tests
