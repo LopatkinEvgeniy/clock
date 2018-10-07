dc8b966768d7762aecaaa5099266169050fa6e9d
elopatkin <javany25@mail.ru>
2018-10-07 22:45:19 +0300

    add comments for timer/ticker

--- ticker.go
+++ ticker.go
@@ -2,6 +2,7 @@ package clock
 
 import "time"
 
+// Ticker is an interface that represents both real and mock tickers.
 type Ticker interface {
 	Chan() <-chan time.Time
 	Stop()
@@ -10,22 +11,27 @@ type Ticker interface {
 var _ Ticker = realTicker{}
 var _ Ticker = mockTicker{}
 
+// realTicker is just a time.Ticker's shallow wrapper.
 type realTicker struct {
 	*time.Ticker
 }
 
+// Chan implementats Ticker.
 func (t realTicker) Chan() <-chan time.Time {
 	return t.C
 }
 
+// mockTicker is just an internalTimer's shallow wrapper.
 type mockTicker struct {
 	*internalTimer
 }
 
+// Chan implementats Ticker.
 func (t mockTicker) Chan() <-chan time.Time {
 	return t.ch
 }
 
+// Stop implementats Ticker.
 func (t mockTicker) Stop() {
 	t.clock.stopTimer(t.internalTimer)
 }
--- timer.go
+++ timer.go
@@ -4,6 +4,7 @@ import (
 	"time"
 )
 
+// Timer is an interface that represents both real and mock timers.
 type Timer interface {
 	Chan() <-chan time.Time
 	Reset(d time.Duration) bool
@@ -13,26 +14,32 @@ type Timer interface {
 var _ Timer = realTimer{}
 var _ Timer = mockTimer{}
 
+// realTimer is just a time.Timer's shallow wrapper.
 type realTimer struct {
 	*time.Timer
 }
 
+// Chan implementats Timer.
 func (t realTimer) Chan() <-chan time.Time {
 	return t.C
 }
 
+// mockTimer is just an internalTimer's shallow wrapper.
 type mockTimer struct {
 	*internalTimer
 }
 
+// Chan implementats Timer.
 func (t mockTimer) Chan() <-chan time.Time {
 	return t.ch
 }
 
+// Stop implementats Timer.
 func (t mockTimer) Stop() bool {
 	return t.clock.stopTimer(t.internalTimer)
 }
 
+// Reset implementats Timer.
 func (t mockTimer) Reset(d time.Duration) bool {
 	return t.clock.resetTimer(t.internalTimer, d)
 }
